05697f1a5bb7a478738b036c3ada5e30dc89fa7f
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2023-09-13 19:55:34 -0300

    feat: styling register page

--- src/components/InputWithLabel/index.tsx
+++ src/components/InputWithLabel/index.tsx
@@ -2,9 +2,9 @@ import { splitProps } from "solid-js";
 
 const Input = (_props) =>{
       const [props, rest] = splitProps(_props, ["label"]);
-return <fieldset>
-        <label>{props.label}</label>
-        <input {...rest}/>
+return <fieldset class="flex flex-col">
+        <label class="mb-2">{props.label}</label>
+        <input class="border-b-black border-b-2 outline-none" {...rest}/>
     </fieldset>
 }
 
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,8 +1,8 @@
 import { createStore } from "solid-js/store";
 import matrixcs from "matrix-js-sdk";
-import { createMemo, createSignal } from "solid-js";
+import { Match, Show, Switch, createMemo, createSignal } from "solid-js";
 import crypto from "crypto";
-import GReCaptch from "solid-grecaptcha"
+import GReCaptch from "solid-grecaptcha";
 import Button from "../../components/Button";
 import Input from "../../components/InputWithLabel";
 interface ErrorMatrixRegister {
@@ -22,6 +22,9 @@ const Register = () => {
     userName: "",
     password: "",
     server: "",
+    session:"",
+    confirmPassword:"",
+    email:""
   });
   const [verifyTypesofFlows, setVerifyTypeOfFlows] =
     createSignal<{ type: string }[]>();
@@ -31,7 +34,7 @@ const Register = () => {
   const [stagesCompleted, setStagesCompleted] = createSignal<
     Array<"m.login.email.identity" | "m.login.recaptcha">
   >([]);
-  const [session, setSession] = createSignal("");
+  const [loading, setLoading] = createSignal(false);
   const [recaptchat, setrecaptchat] = createSignal("");
   const createClient = createMemo(() => {
     return matrixcs.createClient({ baseUrl: fields.server });
@@ -41,11 +44,7 @@ const Register = () => {
     try {
       const stage = currentStage();
       if (stage === "m.login.email.identity") {
-         await client.register(
-          fields.userName,
-          fields.password,
-          null
-        );
+        await client.register(fields.userName, fields.password, null);
       }
     } catch (e: any) {
       const error: ErrorMatrixRegister = e;
@@ -63,32 +62,26 @@ const Register = () => {
         );
         switch (getFirstStateThatNotCompleted) {
           case "m.login.recaptcha": {
-            setSession(error.data.session);
+            setFields("session",error.data.session);
             setCurrentStage(() => "m.login.recaptcha");
             setrecaptchat(
               () => error.data.params["m.login.recaptcha"]?.public_key
-            ); 
+            );
           }
         }
       }
-      console.log("erroir", e.data);
     }
   };
 
-  const onVerify = (responseCaptch:string) => {
+  const onVerify = (responseCaptch: string) => {
     async function callback() {
-    var client = createClient();
+      var client = createClient();
       try {
-         await client.register(
-          fields.userName,
-          fields.password,
-          null,
-          {
-            type: "m.login.recaptcha",
-            session: session(),
-            response: responseCaptch,
-          }
-        );
+        await client.register(fields.userName, fields.password, null, {
+          type: "m.login.recaptcha",
+          session: fields.session,
+          response: responseCaptch,
+        });
       } catch {
         const randomString = crypto.randomBytes(8).toString("hex");
         const response = await client.requestRegisterEmailToken(
@@ -100,10 +93,10 @@ const Register = () => {
         await client.register(
           fields.userName,
           fields.password,
-          session() || null,
+          fields.session || null,
           {
             type: "m.login.email.identity",
-            email: "",
+            email: fields.email,
             threepidCreds: {
               sid: response.sid,
               client_secret: randomString,
@@ -117,45 +110,81 @@ const Register = () => {
       }
     }
     callback();
-  }
+  };
 
   return (
     <div class="h-full w-full flex items-center justify-center bg-slate-900">
+      <div  class="bg-white rounded-md p-8 pb-9 flex flex-col">
       <form
-      class="bg-white rounded-md p-8 pb-9"
         onSubmit={async (e) => {
           e.preventDefault();
-
+          setLoading(true);
           if (verifyTypesofFlows()) {
-            onSubmit();
+            await onSubmit();
             return;
           }
           const client = createClient();
           const response = await client.loginFlows();
           setVerifyTypeOfFlows(response.flows);
           setCurrentStage(() => "m.login.email.identity");
+
+          setLoading(false);
         }}
       >
-        <Input label="servidor url"value ={fields.server}   onInput={(e) => setFields("server", e.target.value)}/>
-        
-        {currentStage() === "m.login.email.identity" && (
-          <div>
-        <Input label="username"value ={fields.userName}   onInput={(e) => setFields("userName", e.target.value)}/>
-        <Input label="password"value ={fields.password}   onInput={(e) => setFields("password", e.target.value)}/>
+        <div class="mb-5">
 
+        <Input
+          label="servidor url"
+          value={fields.server}
+          onInput={(e) => setFields("server", e.target.value)}
+        />
+        </div>
+
+        {currentStage() === "m.login.email.identity" && (
+          <div class="flex flex-col gap-y-5">
+            <div class="flex  gap-x-2">
+              <Input
+                label="username"
+                value={fields.userName}
+                onInput={(e) => setFields("userName", e.target.value)}
+              />
+              <Input
+                label="E-mail"
+                value={fields.email}
+                onInput={(e) => setFields("email", e.target.value)}
+              />
+            </div>
+            <div class="flex gap-x-2">
+              <Input
+                label="password"
+                value={fields.password}
+                onInput={(e) => setFields("password", e.target.value)}
+              />
+              <Input
+                label="confirmPassword"
+                value={fields.confirmPassword}
+                onInput={(e) => setFields("confirmPassword", e.target.value)}
+              />
+            </div>
           </div>
         )}
-  <div class="pt-5">
-        <Button label="logar"/>
-
-  </div>
+        <div class="pt-5">
+          <Show when={currentStage() !== "m.login.recaptcha"}>
+            <Button label="logar" loading={loading()}>
+              <Switch fallback="">
+                <Match when={!currentStage()}>Verificar servidor</Match>
+                <Match when={currentStage() === "m.login.email.identity"}>
+                  Logar
+                </Match>
+              </Switch>
+            </Button>
+          </Show>
+        </div>
       </form>
       {currentStage() === "m.login.recaptcha" && (
-        <GReCaptch
-          siteKey={recaptchat()}
-          onVerify={onVerify}
-        />
+        <GReCaptch siteKey={recaptchat()} onVerify={onVerify} />
       )}
+      </div>
     </div>
   );
 };
