d8ed7cc2c875243224f1044c9a417302a0a67bde
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2023-09-18 20:59:24 -0300

    feat: add initial dashboard

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,43 +1,126 @@
-
+import { For, Show, Switch, Match, createSignal, createMemo } from "solid-js";
 import { createStore } from "solid-js/store";
 import matrixcs from "matrix-js-sdk";
-const Login = () => {
-    const [fields,setFields] = createStore({
-        userName:"",
-        password:"",
-        server:""
-    })
-    const onSubmit = async() =>{
-        var client = matrixcs.createClient({ baseUrl: fields.server });
-        const response = await client
-            .login("m.login.password", {
-              password: "lucas123jmfdsa@$KJ",
-              initial_device_display_name: "app.nitro.chat: Brave em Linux",
-              identifier: {
-                user: "lucas-teste-matrix-clien",
-                type: "m.id.user",
-              },
-            })
-            console.log(response)
-    }
-    return <form onSubmit={(e)=>{
-        e.preventDefault()
-        onSubmit()
-    }}>
-        <fieldset>
-            <label>servidor url</label>
-            <input onInput={(e)=>setFields("server",e.target.value)}/>
-        </fieldset>
-        <fieldset>
-            <label>username</label>
-            <input onInput={(e)=>setFields("userName",e.target.value)}/>
-        </fieldset>
-        <fieldset>
-            <label>password</label>
-            <input onInput={(e)=>setFields("password",e.target.value)}/>
-        </fieldset>
-        <button>Logar</button>
-    </form>
-}
+import Input from "../../components/InputWithLabel";
+import { useNavigate } from "@solidjs/router";
+import Button from "../../components/Button";
+type flowType = Array<"m.login.email.identity" | "m.login.recaptcha">;
+const options = {
+  "m.login.password": {
+    label: "Senha",
+    type: "m.login.email.identity",
+  },
+  "m.login.application_service": {
+    label: "",
+  },
+};
 
-export default Login
+const Login = () => {
+  const [fields, setFields] = createStore({
+    email: "",
+    password: "",
+    server: "",
+  });
+  const createClient = createMemo(() => {
+    return matrixcs.createClient({ baseUrl: fields.server });
+  });
+  const [loading, setLoading] = createSignal(false);
+  const navigation = useNavigate();
+  const [currentStage, setCurrentStage] = createSignal<
+    "m.login.email.identity" | "m.login.recaptcha"
+  >();
+  const onSubmit = async () => {
+    const client = createClient();
+    const response = await client.login("m.login.password", {
+      password: fields.password,
+      initial_device_display_name: "app.nitro.chat: Brave em Linux",
+      identifier: {
+        address: fields.email,
+        medium: "email",
+        type: "m.id.thirdparty",
+      },
+    });
+    navigation("dashboard");
+    console.log(response);
+  };
+  const [verifyTypesofFlows, setVerifyTypeOfFlows] = createSignal<
+    { type: flowType }[]
+  >([]);
+  return (
+    <div class="h-full w-full flex items-center justify-center bg-slate-900">
+      <div class="bg-white rounded-md p-8 pb-9 flex flex-col">
+        <form
+          onSubmit={async (e) => {
+            var client = createClient();
+            e.preventDefault();
+            if (currentStage()) {
+              await onSubmit();
+              return;
+            }
+            const response = await client.loginFlows();
+            console.log(response);
+            setVerifyTypeOfFlows(response.flows);
+          }}
+        >
+          <div class="mb-5">
+            <Input
+              label="servidor url"
+              value={fields.server}
+              onInput={(e) => setFields("server", e.target.value)}
+            />
+          </div>
+          {verifyTypesofFlows()?.length > 0 && (
+            <select
+              onChange={(e) => {
+                console.log(e.target.value);
+                setCurrentStage(() => e.target.value);
+              }}
+            >
+              <option>selecione uma opção</option>
+              <For each={verifyTypesofFlows()}>
+                {(cat, i) => (
+                  <option value={options[cat.type].type}>
+                    {options[cat.type].label}{" "}
+                  </option>
+                )}
+              </For>
+            </select>
+          )}
+
+          {currentStage() === "m.login.email.identity" && (
+            <div class="flex flex-col gap-y-5">
+              <div class="flex  gap-x-2">
+                <Input
+                  label="E-mail"
+                  value={fields.email}
+                  onInput={(e) => setFields("email", e.target.value)}
+                />
+              </div>
+              <div class="flex gap-x-2">
+                <Input
+                  label="password"
+                  value={fields.password}
+                  onInput={(e) => setFields("password", e.target.value)}
+                />
+              </div>
+            </div>
+          )}
+          <div class="pt-5">
+            <Show when={currentStage() !== "m.login.recaptcha"}>
+              <Button label="logar" loading={loading()}>
+                <Switch fallback="">
+                  <Match when={!currentStage()}>Verificar servidor</Match>
+                  <Match when={currentStage() === "m.login.email.identity"}>
+                    Logar
+                  </Match>
+                </Switch>
+              </Button>
+            </Show>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,16 @@
-import { Route, Router, Routes } from '@solidjs/router'
-import Login from '../pages/Login'
-import { Component } from 'solid-js'
-import Register from '../pages/Register'
+import { Route, Router, Routes } from "@solidjs/router";
+import Login from "../pages/Login";
+import { Component } from "solid-js";
+import Register from "../pages/Register";
 
-const Routers:Component =()=> 
-     <Router>
-<Routes>
-  <Route path="/" component={Login} />
-  <Route path="/register" component={Register} />
-</Routes>
-</Router>
+const Routers: Component = () => (
+  <Router>
+    <Routes>
+      <Route path="/" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/dashboard" />
+    </Routes>
+  </Router>
+);
 
-export default Routers
+export default Routers;
